08cc7d747919852a296dec50f03cfd8178105160
van-whiteman <van.whiteman1@students.mq.edu.au>
2024-09-04 18:41:14 +1000

    two graphs commit

--- week7sgta.R
+++ week7sgta.R
@@ -1,2 +1,4 @@
-wed <- na.omit(wed_1800)
-ggplot(wed_1800,aes(x=year,y=maternal_mortality_rate))+geom_line()+facet_wrap(~country)
+library(ggplot2)
+ggplot(wed_1800,aes(x=year,y=maternal_mortality_rate,colour=country,))+geom_line()
+ggplot(wed_1800,aes(x=year,health_expenditure,colour=country,))+geom_line()
+
