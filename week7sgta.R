8b43645c57024a8034252ad955655f995611a561
van-whiteman <van.whiteman1@students.mq.edu.au>
2024-09-04 18:43:53 +1000

    2 graphs on 1 display

--- week7sgta.R
+++ week7sgta.R
@@ -1,4 +1,7 @@
 library(ggplot2)
-ggplot(wed_1800,aes(x=year,y=maternal_mortality_rate,colour=country,))+geom_line()
-ggplot(wed_1800,aes(x=year,health_expenditure,colour=country,))+geom_line()
+library(gridExtra)
+p1 <- ggplot(wed_1800,aes(x=year,y=maternal_mortality_rate,colour=country,))+geom_line()
+p2 <- ggplot(wed_1800,aes(x=year,health_expenditure,colour=country,))+geom_line()
+grid.arrange(p1,p2,ncol=2)
+
 
